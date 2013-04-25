cf59cec24c15e3b1cd1a721ee507c54863eedba9
Cam Spiers <camspiers@gmail.com>
2013-04-25 16:33:40 +1200

    UPDATEL Set up deals for unit tests and jenkins

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+define('UNIT_TESTING', true);
+define('ECOMMERCE_DEALS_BASE_PATH', dirname(__DIR__));
+define('BASE_PATH', ECOMMERCE_DEALS_BASE_PATH);
+
+if (!file_exists(BASE_PATH . '/vendor/autoload.php')) {
+    echo 'You must first install the vendors using composer.' . PHP_EOL;
+    exit(1);
+}
+
+$loader = require BASE_PATH . '/vendor/autoload.php';
+
+use Symfony\Component\ClassLoader\ClassMapGenerator;
+
+$loader->addClassMap(ClassMapGenerator::createMap(BASE_PATH . '/sapphire'));
+$loader->add('Heystack\Subsystem\Deals\Test', __DIR__);
